1bde93a003d6ab4658cb75b639e1ddcded9ab8fe
Maija Barrins <maija.barrins@gmail.com>
2019-05-01 11:22:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,11 @@
-# require modules here
+require "yaml"
 
 def load_library
-  # code goes here
+  YAML.load_file('emoticons.yml')
 end
 
 def get_japanese_emoticon
-  # code goes here
+  binding.pry
 end
 
 def get_english_meaning
