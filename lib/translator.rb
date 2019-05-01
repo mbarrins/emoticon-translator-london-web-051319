e919548f5a7c7dc9185b4ca836094f24024dadf9
Maija Barrins <maija.barrins@gmail.com>
2019-05-01 11:51:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,21 +2,17 @@ require "pry"
 require "yaml"
 
 def load_library(file)
-  thing = YAML.load_file(file)
-  # puts thing.inspect
-  # binding.pry
-  # get_meaning = thing.map{|k,v| [v[1], k]}.to_h
-  # get_emoticon = thing.map{|k,v| [v[0], v[1]]}.to_h
+  yaml_file = YAML.load_file(file)
   {
-    "get_meaning"=> thing.map{|k,v| [v[1], k]}.to_h,
-    "get_emoticon" => thing.map{|k,v| [v[0], v[1]]}.to_h
+    "get_meaning"=> yaml_file.map{|k,v| [v[1], k]}.to_h,
+    "get_emoticon" => yaml_file.map{|k,v| [v[0], v[1]]}.to_h
   }
 
 end
 
 def get_japanese_emoticon(file, emoticon)
   emoticons = load_library(file)
-  emoticons["get_emoticon"][emoticon]
+  emoticons emoticons["get_emoticon"][emoticon]
 end
 
 def get_english_meaning(file, emoticon)
